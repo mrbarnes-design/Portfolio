87d6989d9941b32f6f49454cbd78a80257f5f926
mrbarnes-design <61233040+mrbarnes-design@users.noreply.github.com>
2024-09-09 14:14:38 +0000

    Finished the mobile menu

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,21 @@
+class MobileMenu {
+    constructor() {
+        this.icon = document
+        .querySelector('.site-header__menu-icon')
+        this.nav = document.querySelector('.site-header__menu-content')
+        this.siteHeader = document.querySelector('.site-header')
+        this.events()
+    }
+
+    events() {
+        this.icon.addEventListener('click', ()=> this.toggleTheMenu())
+    }
+
+    toggleTheMenu() {
+        this.nav.classList.toggle('site-header__menu-content--is-visible')
+        this.siteHeader.classList.toggle('site-header--is-expanded')
+        this.icon.classList.toggle('site-header__menu-icon--close-x')
+    }
+}
+
+export default MobileMenu
