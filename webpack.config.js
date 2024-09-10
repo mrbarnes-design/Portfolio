8dd377d590c59fddeceaac4f0a648f469c972f16
mrbarnes-design <61233040+mrbarnes-design@users.noreply.github.com>
2024-09-10 07:36:40 +0000

    Making progess with buuild process

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 
-const postCSSPlugins = [
-  require("postcss-import"),
-  require("postcss-mixins"),
-  require("postcss-simple-vars"),
-  require("postcss-nested"), 
-  require("postcss-hexrgba"),
-  require("autoprefixer")]
+const postCSSPlugins = [require("postcss-import"), require("postcss-mixins"), require("postcss-simple-vars"), require("postcss-nested"), require("postcss-hexrgba"), require("autoprefixer")]
 
-module.exports = {
+let cssConfig = {
+  test: /\.css$/i,
+  use: [
+    { loader: "css-loader", options: { url: false } },
+    { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }
+  ]
+}
+
+let config = {
   entry: "./app/assets/scripts/App.js",
-  output: {
+  module: {
+    rules: [cssConfig]
+  }
+}
+
+if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader")
+  config.output = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app")
-  },
-  devServer: {
+  }
+  config.devServer = {
     watchFiles: ["app/**/*.html"],
     static: {
       directory: path.join(__dirname, "app"),
@@ -22,14 +34,25 @@ module.exports = {
     },
     hot: true,
     port: 3000
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", { loader: "css-loader", options: { url: false } }, { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
-      }
-    ]
   }
+  config.mode = "development"
 }
+
+if (currentTask == "build") {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  config.output = {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "docs"),
+    clean: true
+  }
+  config.mode = "production"
+  config.optimization = {
+    splitChunks: { chunks: "all" },
+    minimize: true,
+    minimizer: [`...`, new CssMinimizerPlugin()]
+  }
+  config.plugins = [new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" })]
+}
+
+module.exports = config
